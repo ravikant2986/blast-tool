fa2dfb2d474f2260c78ce37db2d6859415b9c323
ravikant2986 <ravikant@nascenture.com>
2018-03-16 15:18:08 +0530

    updates to deploy to herokspp

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
-var express = require('express');
-var path = require('path')
-var app = express();
+var http = require('http');
 
-app.use(express.static(path.resolve(__dirname, "/dist")));
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8080);
 
-app.set('port', process.env.PORT || 3001);
-app.listen(app.get('port'), function() {
- console.log("listening to Port", app.get("port"));
-});
+console.log('Server started');
